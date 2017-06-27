b2efdc11eeb7fadc057b9f9bd2e84f0ec835ba90
Zach Morse <zacharymorse@gmail.com>
2017-06-26 22:01:02 -0500

    form validation working, beginning work on game ending conditions

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const sessionConfig = require("./sessionConfig");
 const fs = require("file-system");
+const expressValidator = require("express-validator");
 const app = express();
 const port = process.env.PORT || 7500;
 const words = fs
@@ -13,25 +14,24 @@ const words = fs
   .toLowerCase()
   .split("\n");
 
-// set view engine:
+// --- set view engine:
 
 app.engine("mustache", mustacheExpress());
 app.set("views", "./public");
 app.set("view engine", "mustache");
 
-// middleware
+// --- middleware
 
 app.use("/", express.static("./public"));
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(session(sessionConfig));
-
-// app.use("/", function(req, res, next) {
-//   next();
-// });
 
 // --- game variables
 
-const mysteryWord = words[Math.floor(Math.random() * 200000)].split("");
+const mysteryWord = words[Math.floor(Math.random() * 200000)]
+  .toUpperCase()
+  .split("");
 var displayArray = (function() {
   var dummyArray = [];
   var arrayLength = mysteryWord.length;
@@ -45,35 +45,56 @@ var game = {
   guessAmount: 8,
   mysteryWord: mysteryWord,
   lettersGuessed: [],
+  userDisplayGuessed: "",
   displayArray: displayArray,
-  userDisplayString: displayArray.join(" ")
+  userDisplayString: displayArray.join(" "),
+  statusMessage: ""
 };
 
 console.log(game);
 
-// ROUTES
+// --- INPUT VALIDATION
+
+// --- ROUTES
 
 app.get("/", function(req, res) {
   res.render("index", { game: game });
 });
 
 app.post("/guess", function(req, res) {
-  var userGuess = req.body.guess;
+  var userGuess = req.body.guess.toUpperCase();
+  game.statusMessage = " ";
 
-  mysteryWord.forEach(function(letter, index) {
-    if (userGuess === letter) {
-      game.displayArray[index] = mysteryWord[index];
+  function inputValidator() {
+    req.checkBody(userGuess).isAlpha();
 
-      console.log("LINE 67, game.displayArray: ", game.displayArray);
+    if (userGuess.length == 0) {
+      game.statusMessage = "You must enter a character";
+      res.redirect("/");
+    } else if (game.lettersGuessed.indexOf(userGuess) >= 0) {
+      game.statusMessage = "Letter already guessed, enter different letter";
+      res.redirect("/");
+    } else if (mysteryWord.indexOf(userGuess) < 0) {
+      game.guessAmount -= 1;
+      game.lettersGuessed.push(userGuess);
+      res.redirect("/");
+    } else {
+      game.lettersGuessed.push(userGuess);
+      mysteryWord.forEach(function(letter, index) {
+        if (userGuess === letter) {
+          game.displayArray[index] = mysteryWord[index];
+        }
+      });
     }
-  });
 
-  if (mysteryWord.indexOf(userGuess) < 0) {
-    game.guessAmount -= 1;
+    var errors = req.validationErrors();
+    console.log("errors: ", errors);
   }
+
+  inputValidator();
+  console.log(game);
   game.userDisplayString = game.displayArray.join(" ");
-  game.userDisplayString = game.userDisplayString.toUpperCase();
-  game.lettersGuessed.push(req.body.guess);
+  game.userDisplayGuessed = game.lettersGuessed.join(" ");
   res.redirect("/");
 });
 
